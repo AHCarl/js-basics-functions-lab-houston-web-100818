59daa95d40290eba3b562009cbf4615f62628902
Alexander Carlin <ahcarlin@gmail.com>
2018-11-16 05:32:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 // Code your solution in this file!
 const hq = 42;
+const blockFeet = 264;
 
 function distanceFromHqInBlocks(blocks) {
   return Math.abs(blocks - hq)
 }
 
 function distanceFromHqInFeet(blocks) {
-  return distanceFromHqInBlocks(blocks) * 264
+  return distanceFromHqInBlocks(blocks) * blockFeet
+}
+
+function distanceTravelledInFeet(bloc) {
+  
 }
